1963c121331618f3b17713571651e745e0b9a218
Samuel Olabode <Solabode499@gmail.com>
2025-11-24 12:20:58 -0700

    Add EJs config

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+
+const ejsMate = require('ejs-mate');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended : true}));
+app.engine('ejs', ejsMate);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Bilad Cleaning Service Home Page');
 });
