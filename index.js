228613358da7294d8fc0aad341b44a3c671ea13e
Samuel Olabode <Solabode499@gmail.com>
2025-11-25 17:53:32 -0700

    `Reordered and refactored flash message rendering`

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ const sessionConfig = {
     }
 };
 
-app.use(session(sessionConfig));
-app.use(flash());
+app.use(flash())
 
+app.use(session(sessionConfig));
 app.use((req, res, next)=>{
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
